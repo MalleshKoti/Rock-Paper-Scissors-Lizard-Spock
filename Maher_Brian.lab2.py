0df8b2c7dad410d79633e27833fb734dc8062f69
Brian Maher <bmaher91@gmail.com>
2015-03-02 10:56:57 -0700

    fixed syntax errors, needed to call Element.name as a function

--- Maher_Brian.lab2.py
+++ Maher_Brian.lab2.py
@@ -1,7 +1,3 @@
-# Imports
-import pygame
-
-
 class Element:
 	def __init__(self, name):
 		self._name = name
@@ -9,75 +5,75 @@ class Element:
 	def name(self):
 		return self._name
 
-	def compareTo(self, element):
+	def compareTo(self, Element):
 		raise NotImplementedError('Not yet implemented')
 
 
 class Rock(Element):
-	def compareTo(self, element):
-		if element.name == 'Rock':
+	def compareTo(self, Element):
+		if Element.name() == 'Rock':
 			return 'Rock equals Rock', 'Tie'
-		elif element.name == 'Lizard':
+		elif Element.name() == 'Lizard':
 			return 'Rock crushes Lizard', 'Win'
-		elif element.name == 'Scissors':
+		elif Element.name() == 'Scissors':
 			return 'Rock crushes Scissors', 'Win'
-		elif element.name == 'Paper':
+		elif Element.name() == 'Paper':
 			return 'Paper covers Rock', 'Lose'
-		elif element.name == 'Spock':
+		elif Element.name() == 'Spock':
 			return 'Spock vaporizes Rock', 'Lose'
 
 
 class Paper(Element):
-	def compareTo(self, element):
-		if element.name == 'Paper':
+	def compareTo(self, Element):
+		if Element.name() == 'Paper':
 			return 'Paper equals Paper', 'Tie'
-		elif element.name == 'Rock':
+		elif Element.name() == 'Rock':
 			return 'Paper covers Rock', 'Win'
-		elif element.name == 'Spock':
+		elif Element.name() == 'Spock':
 			return 'Paper disproves Spock', 'Win'
-		elif element.name == 'Scissors':
+		elif Element.name() == 'Scissors':
 			return 'Scissors cut Paper', 'Lose'
-		elif element.name == 'Lizard':
+		elif Element.name() == 'Lizard':
 			return 'Lizard eats Paper', 'Lose'
 
 
 class Scissors(Element):
-	def compareTo(self, element):
-		if element.name == 'Scissors':
+	def compareTo(self, Element):
+		if Element.name() == 'Scissors':
 			return 'Scissors equals Scissors', 'Tie'
-		elif element.name == 'Paper':
+		elif Element.name() == 'Paper':
 			return 'Scissors cut Paper', 'Win'
-		elif element.name == 'Lizard':
+		elif Element.name() == 'Lizard':
 			return 'Scissors decapitate Lizard', 'Win'
-		elif element.name == 'Spock':
+		elif Element.name() == 'Spock':
 			return 'Spock smashes Scissors', 'Lose'
-		elif element.name == 'Rock':
+		elif Element.name() == 'Rock':
 			return 'Rock crushes Scissors', 'Lose'
 
 
 class Lizard(Element):
-	def compareTo(self, element):
-		if element.name == 'Lizard':
+	def compareTo(self, Element):
+		if Element.name() == 'Lizard':
 			return 'Lizard equals Lizard', 'Tie'
-		elif element.name == 'Spock':
+		elif Element.name() == 'Spock':
 			return 'Lizard poisons Spock', 'Win'
-		elif element.name == 'Paper':
+		elif Element.name() == 'Paper':
 			return 'Lizard eats Paper', 'Win'
-		elif element.name == 'Rock':
+		elif Element.name() == 'Rock':
 			return 'Rock crushes Lizard', 'Lose'
-		elif element.name == 'Scissors':
+		elif Element.name() == 'Scissors':
 			return 'Scissors decapitate Lizard', 'Lose'
 
 
 class Spock(Element):
-	def compareTo(self, element):
-		if element.name == 'Spock':
+	def compareTo(self, Element):
+		if Element.name() == 'Spock':
 			return 'Spock equals Spock', 'Tie'
-		elif element.name == 'Scissors':
+		elif Element.name() == 'Scissors':
 			return 'Spock smashes Scissors', 'Win'
-		elif element.name == 'Rock':
+		elif Element.name() == 'Rock':
 			return 'Spock vaporizes Rock', 'Win'
-		elif element.name == 'Lizard':
+		elif Element.name() == 'Lizard':
 			return 'Lizard poisons Spock', 'Lose'
-		elif element.name == 'Paper':
+		elif Element.name() == 'Paper':
 			return 'Paper disproves Spock', 'Lose'
